5c8f304bcd8ba67e24c9ea983c64ac52b040ccd1
st dissanayake <it24101867@my.sliit.lk>
2025-08-21 15:44:23 +0530

    Initial commit

---
+++ library-management-system/src/test/java/lk/library/library_management_system/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package lk.library.library_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
